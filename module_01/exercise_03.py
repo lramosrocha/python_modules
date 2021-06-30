ff8024de5c44f8b96012e3b880549881ec73c3d7
Lokivir <lokivirvorel@gmail.com>
2021-06-30 09:25:54 +0200

    module 1

---
+++ module_01/exercise_03.py
@@ -0,0 +1,33 @@
+# +	plus			# addition
+# -	minus			# subtraction
+# /	slash			# division
+# *	asterisk		# multiplication
+# %	percent			# modulo (shows remainder of division)
+# <	less- than		# boolean, if left number is less-than the right one
+# >	greater- than		# same as the one before, but if the left number is greater
+# <=	less- than- equal	# boolean, if left number is less-than or equal to the right one
+# >=	greater- than- equal	# same as the one before, but if the left number is greater or equal
+
+print("I will now count my chickens:")		# prints text
+
+print("Hens", 25 + 30 / 6)			# prints text with result of math equation
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Nt I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7")
+
+print(3 + 2 < 5 - 7)				# prints boolean of compared equations
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > - 2)
+print("Is it greater or equal?", 5 >= - 2)
+print("Is it less or equal?", 5 <= - 2)
---
+++ module_01/exercise_04.py
@@ -0,0 +1,29 @@
+# declare & initialize variables
+cars = 100;
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+
+# prints text with variable contents
+print("There are", cars, "cars available.")
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car, "in each car.")
+
+
+# Study Drill
+# "NameError: name 'car_pool_capacity' is not defined
+# in line 8 we declared the variable 'carpool_capacity' and not 'car_pool_capacity'
+# 1. It's not necessary, but if only 4 is used, then the decimals would be cut off
+# 2. 
+# 3. 
+# 4. 
+# 5. 
+# 6. 
