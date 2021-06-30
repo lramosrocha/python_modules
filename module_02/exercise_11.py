07a44b676485bcac0b7813d65d29b86e830e36fc
Lokivir <lokivirvorel@gmail.com>
2021-06-30 14:49:49 +0200

    changed prints, added ','

--- module_02/exercise_11.py
+++ module_02/exercise_11.py
@@ -1,6 +1,9 @@
-age = input("How old are you? ")
-height = input("How tall are you? ")
-weight = input("How much do you weigh? ")
+print("How old are you?"),
+age = input()
+print("How tall are you?"),
+height = input()
+print("How much do you weigh?"),
+weight = input()
 
 print("So, you're %r old, %r tall and %r heavy." % (
     age, height, weight))
