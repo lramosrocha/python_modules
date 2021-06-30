f6333a38d378f31f09500b7c8214cac22eff3e6d
Lokivir <lokivirvorel@gmail.com>
2021-06-30 11:32:56 +0200

    raw practice code

---
+++ module_01/module_01_practice.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python5
+# -*- coding: utf-X -*-
+
+# Filename: module_1_practice.py
+
+# Practice exercise for exercises 1-10
+# from the book Learn Python the Hard Way (third edition)
+
+# This is an exercise for Module 1
+Please fix each error in this program!
+
+work_days = "Mon Tue Wed Thu Fri
+weekend_days "Sat Sun"
+all_days = work_days + " " weekend_days
+
+print("All days are: %s" % all_days
+
+num_days = '7'
+
+print("A week has %d days and these are: %s"  (num_days, all_days)
