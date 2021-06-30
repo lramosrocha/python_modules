c397909105e5ade138137b9791d40b6a10ad2a78
Lokivir <lokivirvorel@gmail.com>
2021-06-30 14:59:19 +0200

    errors fixed

--- module_02/module_02_practice.py
+++ module_02/module_02_practice.py
@@ -6,19 +6,28 @@
 # Practice exercise for exercises 11-14
 # from the book Learn Python the Hard Way (third edition)
 
-from sys import arg
+from sys import argv
 
-script, arg1 arg2 = argv
+script, arg1, arg2 = argv
 
 print("""
   Script    : %s
   Argument 1: %s
-  Argument 2: 
+  Argument 2: %s
 """
 % (script, arg1, arg2))
 
-input_number = int(input("Please enter a number: ")
+input_number = int(input("Please enter a number: "))
 
 # Multiply 8 with the input_number variable and print the result
-calc_result = 8 input_number
-print("8 * %d = %d" (input_number, calc_result))
+calc_result = 8 * input_number
+print("8 * %d = %d" % (input_number, calc_result))
+
+
+# Errors
+# ln 9: 'from sys import arg' > 'from sys import argv
+# ln 11: missing ',' (comma) after arg1
+# ln 16: missing %s
+# ln 20: missing bracket after '")'
+# ln 23: missing '*'
+# ln 24: missing '%'
