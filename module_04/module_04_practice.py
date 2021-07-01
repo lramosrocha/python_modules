0e64bcaa4ffbc2439c176ee99575b8b1a9861e2a
Lokivir <lokivirvorel@gmail.com>
2021-07-01 14:08:08 +0200

    raw practice code

---
+++ module_04/module_04_practice.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Filename: module_4_practice.py
+
+# Practice exercise for exercises 18-24
+# from the book Learn Python the Hard Way (third edition)
+
+
+def add_numbers(arg1 arg2):
+    return arg1 + arg2
+
+def print_something(text):
+    print("Hello... %s" % text)
+
+def print_something():
+    print("Hello World!")
+
+num1 = 34.643254353
+num2 = 56.845345456
+result add_numbers(num1, num2)
+
+print("Calculation: %f + %i = %s" % (num1, num2, result))
