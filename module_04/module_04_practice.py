df47ac11616b7929066923146d9c2b7bd6393390
Lokivir <lokivirvorel@gmail.com>
2021-07-01 14:25:28 +0200

    errors 'fixed'

--- module_04/module_04_practice.py
+++ module_04/module_04_practice.py
@@ -7,17 +7,26 @@
 # from the book Learn Python the Hard Way (third edition)
 
 
-def add_numbers(arg1 arg2):
+def add_numbers(arg1, arg2):
     return arg1 + arg2
 
-def print_something(text):
-    print("Hello... %s" % text)
+# def print_something(text):
+#     print("Hello... %s" % text)
 
 def print_something():
     print("Hello World!")
 
 num1 = 34.643254353
 num2 = 56.845345456
-result add_numbers(num1, num2)
+result = add_numbers(num1, num2)
 
-print("Calculation: %f + %i = %s" % (num1, num2, result))
+print("Calculation: %r + %r = %r" % (num1, num2, result))
+print_something()
+
+
+# Errors
+# ln 10: missing ',' between arguments
+# ln 13/14: only one function
+# ln 21: missing '=' after 'result', variable is not declared/initialized
+# ln 23: '%i' instead of '%f'
+# ln 23: '%s' or '%r' would be the best answer, to display all digits
